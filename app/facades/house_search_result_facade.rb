9403254cc3db2d42c78348bcb5a8318b62e8ab5c
James Cape <james.cape@gmail.com>
2019-07-19 10:54:41 -0600

    Uses house_member model in facade, which allows access in view

--- app/facades/house_search_result_facade.rb
+++ app/facades/house_search_result_facade.rb
@@ -7,7 +7,11 @@ class HouseSearchResultFacade
   def house_members
     response = Faraday.get("http://westerosapi.herokuapp.com/api/v1/house/#{@house}?api_key=#{ENV['API_KEY']}")
 
-    JSON.parse(response.body, symbolize_names: true)[:data][0][:attributes][:members]
+    members = JSON.parse(response.body, symbolize_names: true)[:data][0][:attributes][:members]
+
+    members.map do |member|
+      HouseMember.new(member)
+    end
   end
 
 
