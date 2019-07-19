5c6b979af83306ca900185c752a28944dc5f07ec
James Cape <james.cape@gmail.com>
2019-07-19 10:45:46 -0600

    Adds house_members search results facade

--- app/facades/house_search_result_facade.rb
+++ app/facades/house_search_result_facade.rb
@@ -4,8 +4,10 @@ class HouseSearchResultFacade
     @house = house
   end
 
-  def find_house(house)
-    response = Faraday.get("http://westerosapi.herokuapp.com/api/v1/house/#{house}?api_key=#{ENV['API_KEY']}")
+  def house_members
+    response = Faraday.get("http://westerosapi.herokuapp.com/api/v1/house/#{@house}?api_key=#{ENV['API_KEY']}")
+
+    JSON.parse(response.body, symbolize_names: true)[:data][0][:attributes][:members]
   end
 
 
